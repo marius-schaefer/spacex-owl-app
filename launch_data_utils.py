8ecb97221b75061e303fe6734d6dda71b261c08d
marius-schaefer <marius.schaefer2005@gmail.com>
2021-01-08 18:16:21 +0200

    Added Error Handling, to the first function

--- launch_data_utils.py
+++ launch_data_utils.py
@@ -3,46 +3,49 @@ import json
 import pandas as pd
 
 def launch_info():
-    x = input('> ')
-    url = 'https://api.spacex.land/graphql/'
-    query = """{
-    launch(id: """ + '"'+ x + '"' + """) {
-        rocket {
-        rocket_name
-        rocket_type
+    try:
+        x = input('> ')
+        url = 'https://api.spacex.land/graphql/'
+        query = """{
+        launch(id: """ + '"'+ x + '"' + """) {
+            rocket {
+            rocket_name
+            rocket_type
+            }
+            launch_date_utc
+            launch_year
+            launch_site {
+            site_name
+            }
+            launch_success
+            details
         }
-        launch_date_utc
-        launch_year
-        launch_site {
-        site_name
-        }
-        launch_success
-        details
-    }
-    }"""
+        }"""
 
 
-    res = requests.post(url, json={'query' : query})
-    data = json.loads(res.text)
-    #fdata stands for Formated Data, it requires less input, etc I don't really know the terminology for this
-    fdata = data['data']['launch']
+        res = requests.post(url, json={'query' : query})
+        data = json.loads(res.text)
+        #fdata stands for Formated Data, it requires less input, etc I don't really know the terminology for this
+        fdata = data['data']['launch']
 
-    rocket = fdata['rocket']['rocket_name']
-    launch_date = fdata['launch_date_utc']
-    launch_site = fdata['launch_site']['site_name']
-    launch_success = fdata['launch_success']
-    details = fdata['details']
+        rocket = fdata['rocket']['rocket_name']
+        launch_date = fdata['launch_date_utc']
+        launch_site = fdata['launch_site']['site_name']
+        launch_success = fdata['launch_success']
+        details = fdata['details']
 
-    return f"""
-Rocket: {rocket}
-Launch Date: {launch_date}
-Launch Site: {launch_site}
-Launch Success: {launch_success}
+        return f"""
+    Rocket: {rocket}
+    Launch Date: {launch_date}
+    Launch Site: {launch_site}
+    Launch Success: {launch_success}
 
-Details:
-    
-    {details} """
-
+    Details:
+        
+        {details} """
+    except:
+        print('Sorry, the launch that you requested has not been launched yet, please try again!')
+        
 
 def launch_info_var():
     x = input('> ')
