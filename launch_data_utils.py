95847b84727c65df6e8135cf6117fd220f484af2
marius-schaefer <marius.schaefer2005@gmail.com>
2021-01-08 13:54:53 +0200

    Added module Launch_data_utils, inside of that module a function for getting data from a launch has been added, At the same time Main.py was also added, in it there is now the function from launch_data_utils module, allowing the user to get data in their cli when running this programm

---
+++ launch_data_utils.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import pandas as pd
+
+def launch_info():
+    x = input('> ')
+    url = 'https://api.spacex.land/graphql/'
+    query = """{
+    launch(id: """ + '"'+ x + '"' + """) {
+        rocket {
+        rocket_name
+        rocket_type
+        }
+        launch_date_utc
+        launch_year
+        launch_site {
+        site_name
+        }
+        launch_success
+        details
+    }
+    }"""
+
+
+    res = requests.post(url, json={'query' : query})
+    data = json.loads(res.text)
+    #fdata stands for Formated Data, it requires less input, etc I don't really know the terminology for this
+    fdata = data['data']['launch']
+
+    rocket = fdata['rocket']['rocket_name']
+    launch_date = fdata['launch_date_utc']
+    launch_site = fdata['launch_site']['site_name']
+    launch_success = fdata['launch_success']
+    details = fdata['details']
+
+    return f"""
+Rocket: {rocket}
+Launch Date: {launch_date}
+Launch Site: {launch_site}
+Launch Success: {launch_success}
+
+Details:
+    
+    {details} """
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from launch_data_utils import *
+
+
+print(launch_info())
